bf670c9809947a5cf0da592f91bebd94e3e7b1ee
Yasra Zhafirah <97837528+spacetimist@users.noreply.github.com>
2023-10-31 11:07:52 +0700

    Update start.h

--- src/command/start.h
+++ src/command/start.h
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../ADT/mesinkata.h"
-#include "../ADT/array.h"
-#include "../ADT/StackV2.h"
-
 #ifndef START_H
 #define START_H
 
-void START(Arr *arrgame, StackW *history);
 
 #endif
