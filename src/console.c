f453361bc6aa0425f5b81bcac25b0d955f791a01
kayla1410 <130771373+kayla1410@users.noreply.github.com>
2023-11-06 11:58:17 +0700

    ,,

--- src/console.c
+++ src/console.c
@@ -112,13 +112,108 @@ void playlistAdd(){
 
     printf("Masukkan Nama Penyanyi yang dipilih : ");
     startInputWord();
+    // kalau nama penyanyi benar
+    // menampilkan daftar album
+    printf("Daftar Album oleh %s : \n", wordToString(namapenyanyi));
+    for (int i = 0; i <= lengthlist; i++) {
+        printf("    %d. %s", i, (list).buffer[i]);
+    }
     printf("\n\n");
-    
 
-/* kalo salah input */
-    printf("Penyanyi ... tidak ada dalam daftar. Silakan coba lagi.")
+    // kalau nama penyanyi salah
+    printf("Penyanyi ... tidak ada dalam daftar. Silakan coba lagi.");
+
+    printf("Masukkan Judul Album yang dipilih : %s", wordToString(judulalbum));
+    startInputWord();
+    // kalau judul album benar
+    // menampilkan daftar lagu
+    printf("Daftar Lagu Album %s oleh %s : \n", wordToString(namapenyanyi), wordToString(judulalbum));
+    for (int i = 0; i <= lengthlist; i++) {
+        printf("    %d. %s", i, (list).buffer[i]);
+    }
+    printf("\n\n");
+
+    // kalau salah judul
+    printf("Album ... tidak ada dalam daftar. Silakan coba lagi.\n");
+
+    if (//ADDSONG) {
+        printf("Masukkan ID Lagu yang dipilih : ");
+        startInputWord();
+
+        /* kalo salah input */
+        printf("ID Lagu ... tidak ada dalam daftar. Silakan coba lagi.");
+
+        printf("\n\n");
+
+        printf("Daftar Playlist Pengguna : \n");
+        for (int i = 0; i <= lengthlist; i++) {
+            printf("    %d. %s", i, (list).buffer[i]);
+
+        }
+
+        printf("\n\n");
+
+        printf("Masukkan ID PLaylist yang dipilih : ");
+        startInputWord();
+
+        // kalo benar
+        printf("Lagu dengan judul “%s” pada album %s oleh penyanyi %s berhasil ditambahkan ke dalam playlist %s.")
+        /* kalo salah input */
+        printf("ID Playlist ... tidak ada dalam daftar. Silakan coba lagi.");
+
+    }
+    else if (//ADDSONG){
+        printf("Daftar Playlist Pengguna : \n");
+        for (int i = 0; i <= lengthlist; i++) {
+            printf("    %d. %s", i, (list).buffer[i]);
+
+        }
+
+        printf("\n\n");
+
+        printf("Masukkan ID PLaylist yang dipilih : ");
+        startInputWord();
+
+        // kalo benar
+        printf("Album dengan judul “%s” berhasil ditambahkan ke dalam pada playlist pengguna “%s”");
+        /* kalo salah input */
+        printf("ID Playlist ... tidak ada dalam daftar. Silakan coba lagi.");
+    }
 } 
 
+void playlistSwap(a, b, c){ 
+    //a = playlist
+    //b = urutan awal
+    //c = urutan akhir
+
+    printf("Berhasil menukar lagu dengan nama “...” dengan “...” di playlist “...”");
+    //kalo ga ada ID playlistnya
+    printf("Tidak ada playlist dengan playlist ID ...");
+    //kalo ga ada urutan lagunya
+    printf("Tidak ada lagu dengan urutan ... di playlist “...”");
+}
+
+void playlistRemove(a, b){
+    // a = playlist
+    // b = urutan lagu
+    printf("Lagu ... oleh ... telah dihapus dari playlist ...!");
+    //kalo ga ada ID playlistnya
+    printf("Tidak ada playlist dengan ID ....");
+    //kalo ga ada urutannya di playlist
+    printf("Tidak ada lagu dengan urutan ... di playlist ... !");
+}
+
+void playlistDelete() {
+    printf("Daftar Playlist Pengguna :\n");
+    for (int i = 0; i <= lengthlist; i++) {
+        printf("    %d. %s", i, (list).buffer[i]);
+    }
+
+    printf("Masukkan ID Playlist yang dipilih : ");
+    
+    //kalo ga ada ID Playlist nya di daftar
+    printf("Tidak ada playlist dengan ID 11 dalam daftar playlist pengguna. Silakan coba lagi.");
+}
 
 void HelpBeforeStart() {
     printf("-------------------- [ Menu Help WayangWave ] --------------------");
