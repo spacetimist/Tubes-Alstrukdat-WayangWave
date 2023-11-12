eec9f54d7963614450a0e7e3077708cf6a1f221b
kayla1410 <130771373+kayla1410@users.noreply.github.com>
2023-11-12 17:34:20 +0700

    pindahin playlist

---
+++ src/command/playlist.h
@@ -0,0 +1,21 @@
+#include "../ADT/mesin/mesinkata.h"
+#include "../ADT/mesin/mesinkarakter.h"
+#include "../ADT/mesin/mesinkalimat.h"
+#include "../ADT/map2/map2.h"
+#include "../ADT/word/word.h"
+#include "../ADT/listlinier/listlinier.h"
+#include "../ADT/queue/queue.h"
+#include "../ADT/set/set.h"
+#include "../command/start.h"
+#include "boolean.h"
+
+// playlist
+void createPlaylist(List *playlist);
+
+void playlistAdd(List *singer);
+
+void playlistSwap(a, b, c);
+
+void playlistRemove(a, b);
+
+void playlistDelete() {
