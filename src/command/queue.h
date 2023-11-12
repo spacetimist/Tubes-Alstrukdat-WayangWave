823ecd93cc751d3aaaf1e508e0ec0a85ca8d3d41
unknown <syakiraafn@gmail.com>
2023-11-12 17:44:11 +0700

    update queue.c & queue.h

---
+++ src/command/queue.h
@@ -0,0 +1,24 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+#include "./ADT/mesin/mesinkata.h"
+#include "./ADT/mesin/mesinkarakter.h"
+#include "./ADT/mesin/mesinkalimat.h"
+#include "./ADT/map2/map2.h"
+#include "./ADT/word/word.h"
+#include "./ADT/listlinier/listlinier.h"
+#include "./ADT/queue/queue.h"
+#include "./ADT/set/set.h"
+#include "./command/start.h"
+
+
+void QueueSong(Queue *queue, ListPenyanyi *ls);
+void QueuePlaylist(Queue *queue);
+void QueueSwap(Queue *queue, int id, int id2);
+void QueueRemove(Queue *queue, int id);
+void QueueClear(Queue *queue);
+
+#endif
