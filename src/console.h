83dbed7ec5a7223b3b9f45695de5d84b54eb87bd
root <root@Skies-HP>
2023-11-19 10:41:27 +0700

    delete console.c, ubah console2 jadi console (termasuk di makefile)

--- src/console.h
+++ src/console.h
@@ -2,25 +2,9 @@
 #include "./ADT/mesin/mesinkarakter.h"
 #include "./ADT/mesin/mesinkalimat.h"
 #include "./ADT/map2/map2.h"
-#include "./ADT/word/word.h"
-#include "./ADT/listlinier/listlinier.h"
-#include "./ADT/queue/queue.h"
-#include "./ADT/set/set.h"
 #include "./command/start.h"
+#include "./command/list.h"
+#include "./ADT/queue/queue.h"
+#include "./ADT/stack/stack.h"
 
-// queue
-void QueueSong(Queue *queue, List *singer);
-void QueuePlaylist(Queue *queue);
-void QueueSwap(Queue *queue, int id1, int id2);
-void QueueRemove(Queue *queue, int id);
-void QueueClear(Queue *queue);
-
-// playlist
-void createPlaylist(List *playlist);
-
-// help
-void HelpBeforeStart();
-void HelpAfterStart();
-
-//invalid comment
-void invalidComment();
+void banner();
