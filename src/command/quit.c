907286e91a6944517a9939802ccc3cc3e03c4fce
unknown <syakiraafn@gmail.com>
2023-11-19 09:54:44 +0700

    .

---
+++ src/command/quit.c
@@ -0,0 +1,13 @@
+#include "quit.h"
+#include <stdio.h>
+
+void quit(){
+    printf("Apakah kamu ingin menyimpan data sesi sekarang? (Y/N) ");
+    StartInput();
+    if(isInputEqual(Input, "Y")){
+        // save();
+    }else if(isInputEqual(Input, "N")){
+        printf("Kamu keluar dari WayangWave.\nDadah ^_^/\n");
+        exit();
+    }
+}
---
+++ src/command/quit.h
@@ -0,0 +1,13 @@
+#ifndef __QUIT_H__
+#define __QUIT_H__
+
+#include "boolean.h"
+#include <stdio.h>
+#include "../ADT/mesin/mesinkata.h"
+#include "../ADT/mesin/mesinkarakter.h"
+#include "../ADT/mesin/mesinkalimat.h"
+#include "../command/save.h"
+
+void quit();
+
+#endif
